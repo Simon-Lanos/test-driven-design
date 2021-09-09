c01489160ec265021df29da15dd6889c69df063b
Simon LANOS <simon.lanos99@gmail.com>
2021-09-09 10:43:12 +0200

    add actual implementation of prime factors

--- src/PrimeFactors.php
+++ src/PrimeFactors.php
@@ -4,18 +4,21 @@ namespace App;
 
 class PrimeFactors
 {
+    // https://www.fusionswift.com/2010/06/php-prime-factors/
     public static function generate(int $number): array
     {
         if ($number === 1) {
             return [];
         }
 
-        if ($number === 2 || $number === 3) {
-            return [$number];
+        $sqrt = sqrt($number);
+        for ($i = 2; $i <= $sqrt; $i++) {
+            if ($number % $i === 0) {
+                $return = array_merge(self::generate($number/$i), array($i));
+                sort($return, SORT_REGULAR);
+                return $return;
+            }
         }
-
-        if ($number === 4) {
-            return [2, 2];
-        }
+        return [$number];
     }
 }
